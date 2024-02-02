1cae044dfc2aa372c0efa4dda93c965746b5c998
Alabo Excel <66995707+alabo-excel@users.noreply.github.com>
2024-02-02 13:02:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,11 +18,8 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(
-  cors({
-    origin: (_origin, callback) => {
-      callback(null, true);
-    },
-    credentials: true,
+  cors(
+    
 )); 
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({extended: false}));
