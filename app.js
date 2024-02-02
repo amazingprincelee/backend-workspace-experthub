c49fb0b4cb92df555b25d5f40c7e73b378eba52a
Alabo Excel <66995707+alabo-excel@users.noreply.github.com>
2024-02-02 05:15:57 -0800

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, DELETE, PUT"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization ");
+  // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization ");
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
