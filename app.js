77e66ed11044a7db535ff7533cf66b95fc337f40
Alabo Excel <66995707+alabo-excel@users.noreply.github.com>
2024-02-02 05:15:15 -0800

    Update app.js

--- app.js
+++ app.js
@@ -17,10 +17,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(
-  cors(
-    
-)); 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, DELETE, PUT"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization ");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+}); 
+
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'))
